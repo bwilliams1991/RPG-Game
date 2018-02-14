e3b251123199ede045393f1351eb93ff73fae0ad
ben <bwilly91@msn.com>
2018-02-13 18:30:46 -0700

    styling, fight functionality needs impovments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,15 +3,13 @@ $(document).ready(function () {
 
 	// // Varible Declarations
 	// --------------------------------------------------------------------------------------
-	var i;
-	var j;
 
 	// character selections
 	var characterSelected = false;
 	var enemySelected = false;
 
 	// store current choice
-	var theCharacter = {};
+	var character = {};
 	var theEnemy = {};
 
 	// hope this is obvious, one end-game instance is =3 -> win
@@ -29,7 +27,8 @@ $(document).ready(function () {
 			attackPower: 8,
 			counterAttackPower: 10,
 			baseAttack: 8,
-			src: "./assets/images/obi-wan-kenobi.jpg"
+			src: "./assets/images/obi-wan-kenobi.jpg",
+			id: "obi-wan"
 		},
 		{ //character[1].attribute
 			name: "Luke Skywalker",
@@ -37,7 +36,8 @@ $(document).ready(function () {
 			attackPower: 8,
 			counterAttackPower: 5,
 			baseAttack: 8,
-			src: "./assets/images/luke-skywalker.jpg"
+			src: "./assets/images/luke-skywalker.jpg",
+			id: "luke"
 		},
 		{ //character[2].attribute
 			name: "Darth Sidious",
@@ -45,7 +45,8 @@ $(document).ready(function () {
 			attackPower: 8,
 			counterAttackPower: 20,
 			baseAttack: 8,
-			src: "./assets/images/sid.png"
+			src: "./assets/images/sid.png",
+			id: "sid"
 		},
 		{ //character[3].attribute
 			name: "Darth Maul",
@@ -53,19 +54,20 @@ $(document).ready(function () {
 			attackPower: 8,
 			counterAttackPower: 25,
 			baseAttack: 8,
-			src: "./assets/images/DarthMaul.jpeg"
+			src: "./assets/images/DarthMaul.jpeg",
+			id: "maul"
 		}
 	];
 
 	var messages = {
-			attack: "You attacked " + theDefender.name + " for " + theCharacter.attackPower + " damage.",
-			counter: theDefender.name + "attacked you back for " + theDefender.counterAttackPower + "damage.",
-			defeat: "You have defeated " + theDefender.name + ", choose the next combatant.",
-			end: "You have been defeated ... Game Over!",
-			missing: "No enemy to attack, please choose another enemy.",
-			win: "Winner!",
-			character: "Select your character",
-		}
+		attack: "You attacked " + theDefender.name + " for " + theCharacter.attackPower + " damage.",
+		counter: theDefender.name + "attacked you back for " + theDefender.counterAttackPower + "damage.",
+		defeat: "You have defeated " + theDefender.name + ", choose the next combatant.",
+		end: "You have been defeated ... Game Over!",
+		missing: "No enemy to attack, please choose another enemy.",
+		win: "Winner!",
+		character: "Select your character",
+	}
 
 
 	// Functions
@@ -79,68 +81,70 @@ $(document).ready(function () {
 		var imgBtn = $("<img/>", {
 			src: characters[i].src,
 			width: 250,
-			alt: characters[i].name
+			alt: characters[i].name,
+			id: characters[i].id
 		});
 
 		switch (i) {
 			case 0:
 				imgBtn.addClass("img-thumbnail imgBtn-obi available");
-				$('img').attr("id", "obi-wan");
 				break;
 			case 1:
 				imgBtn.addClass("img-thumbnail imgBtn-luke available");
-				$('img').attr("id", "luke");
 				break;
 			case 2:
 				imgBtn.addClass("img-thumbnail imgBtn-sid available");
-				$('img').attr("id", "sid");
 				break;
 			case 3:
-				imgBtn.addClass("img-thumbnail imgBtn-maul available");
-				$('img').attr("id", "maul");
+				imgBtn.addClass("img-thumbnail imgBtn-maul available")
 				break;
 		}
+		$('img').attr("id", imgBtn.id);
 		$("#characterChoose").append(imgBtn);
 
 		// name
-		var nameOver = $("<h4>");
+		var nameOver = $("<span>");
 		// health
-		var healthOver = $("<h5>");
+		var healthOver = $("<span>");
 
 		switch (i) {
 			case 0:
-				nameOver.addClass("nameover-obi");
+				nameOver.addClass("nameover-obi available");
 				break;
 			case 1:
-				nameOver.addClass("nameover-luke");
+				nameOver.addClass("nameover-luke available");
 				break;
 			case 2:
-				nameOver.addClass("nameover-sid");
+				nameOver.addClass("nameover-sid available");
 				break;
 			case 3:
-				nameOver.addClass("nameover-maul");
+				nameOver.addClass("nameover-maul available");
+				break;
+		}
+
+		switch (i) {
+			case 0:
+				healthOver.addClass("healthover-obi available");
+				break;
+			case 1:
+				healthOver.addClass("healthover-luke available");
+				break;
+			case 2:
+				healthOver.addClass("healthover-sid available");
+				break;
+			case 3:
+				healthOver.addClass("healthover-maul available");
 				break;
 		}
 		nameOver.text(characters[i].name);
-		$("#characterChoose").append(nameOver);
-
-		switch (i) {
-			case 0:
-				healthOver.addClass("healthover-obi");
-				break;
-			case 1:
-				healthOver.addClass("healthover-luke");
-				break;
-			case 2:
-				healthOver.addClass("healthover-sid");
-				break;
-			case 3:
-				healthOver.addClass("healthover-maul");
-				break;
-		}
-		// $('h5').attr("id", "health"); // for testing(@-line101)
 		healthOver.text(characters[i].healthPoints);
+
+		$("#characterChoose").append(nameOver);
 		$("#characterChoose").append(healthOver);
+		
+		// $('h5').attr("id", "health"); // for testing(@-line101)
+		
+		
 		$('h5').attr("id", "health"); // line behaves oddly, not adding id "health" to h5 for d.maul when on line line 98 or earlier.?
 	}
 
@@ -157,11 +161,10 @@ $(document).ready(function () {
 
 	// initialize the enemy value 
 	function theDefender(theDefender) {
-		defender.name = theDefender.name;
-		defender.healthPoints = theDefender.healthPoints;
-		defender.counterAttackPower = theDefender.counterAttackPower;
+		theEnemy.name = theDefender.name;
+		theEnemy.healthPoints = theDefender.healthPoints;
+		theEnemy.counterAttackPower = theEnemy.counterAttackPower;
 	}
-
 
 	// Move remaining to defenders Position
 	function moveEnemies() {
@@ -202,8 +205,8 @@ $(document).ready(function () {
 
 	// Process
 	// --------------------------------------------------------------------------------------
-	
-	// confirm restart button is hidden on game start
+
+	// 	// confirm restart button is hidden on game start
 	$("#restart").hide();
 
 
@@ -221,181 +224,185 @@ $(document).ready(function () {
 			characterSelected = true;
 
 			$("#obi-wan").removeClass("available").addClass("chosen");
-			$("#chosen").append(this);
+			// $("#chosen").append(this);
 
 			// Move the remaining characters to the enemies section
 			moveEnemies();
 
-		} else if ((characterSelected == true) && (defenderSelected == false)) {
+		} else if ((characterSelected == true) && (enemySelected == false)) {
+
+		// User selects defender
+		if($("#obi-wan").hasClass("becomeEnemy")) {
+			$("#message").empty();
+
+			theDefender(characters[0]);
+			enemySelected = true;
+
+			// move to defender section
+			$("#obi-wan").removeClass("theEnemy").addClass("defenders");
+			$("#enemies").append(this);
+		}
+		}
+	});
+
+
+
+	$("#luke").on("click", function () {
+		// console.log("Luke Skywalker");
+
+		if (characterSelected == false) {
+			$("#message").empty();
+
+			theCharacter(characters[1]);
+			characterSelected = true;
+
+			$("#luke").removeClass("available").addClass("chosen");
+			// $("#chosen-character").append(this);
+
+			// Move the remaining characters to the enemies section
+			moveEnemies();
+
+		} else if ((characterSelected == true) && (enemySelected == false)) {
 
 			// User selects defender
-			if ($("#obi-wan").hasClass("theEnemy")) {
+		if($("#luke").hasClass("becomeEnemy")) {
 				$("#message").empty();
 
-				theDefender(characters[1]);// fix
-				defenderSelected = true;
+				theDefender(characters[1]);//fix
+				enemySelected = true;
+
+				// move to defender section 
+				$("#luke").removeClass("theEnemy").addClass("defenders");
+				$("#enemies").append(this);
+		}	
+		}
+	});
+
+	$("#sid").on("click", function () {
+		// console.log("Darth Sidious");
+
+		if (characterSelected == false) {
+			$("#message").empty();
+
+			// Set the user's character
+			theCharacter(characters[2]);
+			characterSelected = true;
+
+			// Display the chosen character
+			$("#sid").removeClass("available").addClass("chosen");
+			// $("#chosen").append(this);
+
+			// Move the remaining characters to the enemies section
+			moveEnemies();
+		} else if ((characterSelected == true) && (enemySelected == false)) {
+
+			// User selects defender
+			if ($("#sid").hasClass("becomeEnemy")) {
+				$("#message").empty();
+
+				// Set the user's enemy
+				theDefender(characters[2]); //fix
+				enemySelected = true;
+
+				// Add the character to the defender section 
+				$("#sid").removeClass("theEnemy").addClass("defenders");
+				$("#enemies").append(this);
+			}	
+		}
+	});
+
+	$("#maul").on("click", function () {
+		// console.log("Darth Maul");
+
+		if (characterSelected == false) {
+			$("#message").empty();
+
+			theCharacter(characters[3]);
+			characterSelected = true;
+
+			// Display the chosen character
+			$("#maul").removeClass("available").addClass("chosen");
+			// $("#chosen").append(this);
+
+			// Move the remaining characters to the enemies section
+			moveEnemies();
+		} else if ((characterSelected == true) && (enemySelected == false)) {
+
+			// User selects defender
+			if($("#maul").hasClass("becomeEnemy")) {
+				$("#message").empty();
+
+				// Set the user's enemy
+				theDefender(characters[3]); //fix
+				enemySelected = true;
 
 				// move to defender section
-				$("#obi-wan").removeClass("theEnemy").addClass("defenders");
+				$("#maul").removeClass("theEnemy").addClass("defenders");
 				$("#enemies").append(this);
 			}
 		}
 	});
-	
-
-
-	$("#luke").on("click", function () {
-    // console.log("Luke Skywalker");
-
-    if(characterSelected == false) {
-      $("#message").empty();
-
-      theCharacter(characters[1]);
-      characterSelected = true;
-
-      $("#luke").removeClass("available").addClass("chosen");
-      // $("#chosen-character").append(this);
-
-      // Move the remaining characters to the enemies section
-			moveEnemies();
-			
-    } else if ((characterSelected == true) && (defenderSelected == false)) {
-
-      // User selects defender
-      if($("#luke").hasClass("theEnemy")) {
-        $("#message").empty();
-
-        theDefender(characters[0]);//fix
-        defenderSelected = true;
-
-        // move to defender section 
-        $("#luke").removeClass("theEnemy").addClass("defenders");
-        $("#enemies").append(this);
-      }
-    }
-  });
-
-  $("#sid").on("click", function () {
-    // console.log("Darth Sidious");
-
-    if(characterSelected == false) {
-      $("#message").empty();
-
-      // Set the user's character
-      theCharacter(characters[2]);
-      characterSelected = true;
-
-      // Display the chosen character
-      $("#sid").removeClass("available").addClass("chosen");
-      // $("#chosen").append(this);
-
-      // Move the remaining characters to the enemies section
-      moveEnemies();
-    } else if ((characterSelected == true) && (defenderSelected == false)) {
-
-      // User selects defender
-      if($("#sid").hasClass("theEnemy")) {
-        $("#message").empty();
-
-        // Set the user's enemy
-        theDefender(characters[1]); //fix
-        defenderSelected = true;
-
-        // Add the character to the defender section 
-        $("#sid").removeClass("theEnemy").addClass("defenders");
-        $("#enemies").append(this);
-      }
-    }
-  });
-
-  $("#maul").on("click", function () {
-    // console.log("Darth Maul");
-
-    if(characterSelected == false) {
-      $("#message").empty();
-
-      theCharacter(characters[3]);
-      characterSelected = true;
-
-      // Display the chosen character
-      $("#maul").removeClass("available").addClass("chosen");
-      // $("#chosen").append(this);
-
-      // Move the remaining characters to the enemies section
-      moveEnemies();
-    } else if ((characterSelected == true) && (defenderSelected == false)) {
-
-      // User selects defender
-      if($("#maul").hasClass("theEnemy")) {
-        $("#message").empty();
-
-        // Set the user's enemy
-        theDefender(characters[1]); //fix
-        defenderSelected = true;
-
-       // move to defender section
-        $("#maul").removeClass("theEnemy").addClass("defenders");
-        $("#enemies").append(this);
-      }
-    }
-  });
 
 
 
-// onclick for attack sequence
+	// onclick for attack sequence
 	//counter attack
-		// attack power increase Math.pow?
+	// attack power increase Math.pow?
 	// win/lose
 	// next defender, repeat fight until finish
-	$("#attack-btn").on("click", function() {
-    if (characterSelected && defenderSelected && !gameOver) {
+	$("#attack-btn").on("click", function () {
+		if (characterSelected && enemySelected && !theEnd) {
 
-      // User attacks
-      theDefender.healthPoints -= theCharacter.attackPower;
-      $(".defender-character").html(theDefender.healthPoints);
-      $("#message").html(messages.attack);
+			// User attacks
+			theEnemy.healthPoints -= theCharacter.attackPower;
+			$(".theEnemy").html(theEnemy.healthPoints);
+			$("#message").html(messages.attack);
 
-      // attack power increases
-      theCharacter.attackPower += theCharacter.baseAttack;
+			// attack power increases
+			theCharacter.attackPower += theCharacter.baseAttack;
 
 			// check defender health and counter(if alive)
-			if (theDefender.healthPoints > 0) {
-        theCharacter.healthPoints -=  theDefender.counterAttackPower;
-        $(".chosen").html(theCharacter.healthPoints);
+			if (theEnemy.healthPoints > 0) {
+				theCharacter.healthPoints -= theEnemy.counterAttackPower;
+				$(".chosen").html(theCharacter.healthPoints);
 
-        // Check user health after counter
-        if (theCharacter.healthPoints > 0) {
-          $("#message").append(messages.counter);
-        } else {
-          theEnd = true;
-          $("#message").html(messages.end);
-          $("#restart").show();
-        }
-      } else {
-        // enemy is defeated
-        enemiesDefeated++;
-        defenderSelected = false;
-        $("#message").html(messages.defeat);
-        $(".defender-character").hide();
+				// Check user health after counter
+				if (theCharacter.healthPoints > 0) {
+					$("#message").append(messages.counter);
+				} else {
+					theEnd = true;
+					$("#message").html(messages.end);
+					$("#restart").show();
+				}
+			} else {
+				// enemy is defeated
+				enemiesDefeated++;
+				enemySelected = false;
+				$("#message").html(messages.defeat);
+				$(".defender-character").hide();
 
-        // Check if winning
-        if (enemiesDefeated === 3) {
-          theEnd = true;
-          $("#message").html(messages.win);
-          $("#restart").show();
-        }
-      }
-    } else if (!characterSelected && !theEnd) {
-      $("#message").html(messages.character);
-    } else if (!defenderSelected && !theEnd) {
-      $("#message").html(messages.missing);
-    }
+				// Check if winning
+				if (enemiesDefeated === 3) {
+					theEnd = true;
+					$("#message").html(messages.win);
+					$("#restart").show();
+				}
+			}
+		} else if (!characterSelected && !theEnd) {
+			$("#message").html(messages.character);
+		} else if (!enemySelected && !theEnd) {
+			$("#message").html(messages.missing);
+		}
 
 	});
-	
-// onclick for restart button
-	$("#restart").on("click", function() {
-    resetGame();
+
+	// onclick for restart button
+	$("#restart").on("click", function () {
+		reset();
 	});
-	
+
 });
+
+
+
+// not displaying/updating fight damage values 
